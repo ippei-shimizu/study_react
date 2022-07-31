47027af05e96dacf8271cf8b6884d2575f1a12f6
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-07-31 21:13:38 +0900

    ⚡️ useEffect,useCallbackに関しての説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,20 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer/";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setFoo(foo => foo + 1);
-  };
+  const handleClick = useCallback(
+    () => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -20,8 +25,6 @@ export default function Home() {
       document.body.style.backgroundColor = "";
     };
   }, []);
-
-  console.log(foo);
 
   return (
     <div className={styles.container}>
@@ -31,7 +34,7 @@ export default function Home() {
       </Head>
       <Header />
       <div className={styles.top}>
-        <h1>{foo}</h1>
+        <h1>{count}</h1>
         <button onClick={handleClick}>ボタン</button>
       </div>
       <Main page="index" />
