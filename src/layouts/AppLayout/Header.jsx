92865b8335d012cb48927ecde2a8cf07c9db9d54
ippei-shimizu <ippei.shimizu.32@gmail.com>
2022-08-11 13:26:22 +0900

    vercelでプレビュー環境を確認するため

--- src/layouts/AppLayout/Header.jsx
+++ src/layouts/AppLayout/Header.jsx
@@ -5,6 +5,7 @@ const NAV_ITEMS = [
   { href: "/posts", label: "Posts" },
   { href: "/users", label: "Users" },
   { href: "/comments", label: "Comments" },
+  { href: "/comments", label: "CommentsTest" },
 ];
 
 export const Header = () => {
